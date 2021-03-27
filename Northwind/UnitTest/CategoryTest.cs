98a6054fdf814cbd07595c7c90f725c7f3b6d937
Pablo Ramos <juanpabloramosp@hotmail.com>
2021-03-26 18:43:32 -0600

    Prueba

---
+++ Northwind/UnitTest/CategoryTest.cs
@@ -0,0 +1,37 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTest
+{
+   public class CategoryTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Category category = new Category
+            {
+                CategoryName = "PruebaNotTgyghyfghfghfhfgh"
+            };
+
+            NorthWndContext context = new NorthWndContext();
+            using (UnidadDeTrabajo<Category> unidad = 
+                new UnidadDeTrabajo<Category>(context))
+            {
+
+                bool result; 
+                    unidad.genericDAL.Add(category);
+                result =  unidad.Complete();
+                Assert.True(!result, "Inserciòn correcta");
+
+            }
+
+
+
+        }
+
+    }
+}
